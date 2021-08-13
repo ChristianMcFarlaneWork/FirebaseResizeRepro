4291839ca3dc5d1a0c7c0b46d4a868d85b9df42e
Christian McFarlane <Work.ChristianMcFarlane@gmail.com>
2021-08-13 12:31:06 +1000

    Full Repro Project

---
+++ Assets/Scripts/ImageUploader.cs
@@ -0,0 +1,92 @@
+﻿using Firebase;
+using Firebase.Extensions;
+using Firebase.Storage;
+using UnityEngine;
+
+public class ImageUploader : MonoBehaviour
+{
+
+	#region Variables
+
+	[Header("Required")]
+	[SerializeField] private ScreenshotCamera _screenshotCamera;
+
+	[Header("Settings")]
+	[SerializeField] private LogLevel _loggingLevel = LogLevel.Error;
+	[Space]
+	[SerializeField] private string _bucketPath = "gs://YOUR-FIREBASE-BUCKET/";
+	[SerializeField] private string _destinationPath = "uploadedImage.png";
+	[SerializeField] private Vector2Int _screenshotRes = new Vector2Int(256, 256);
+
+	public FirebaseStorage storage { get; private set; }
+	public StorageReference bucket { get; private set; }
+
+	#endregion
+
+	#region Start
+
+	public void Start()
+	{
+		InitializeFirebase();
+	}
+
+	#endregion
+
+	#region Initialize Firebase + Storage
+
+	private void InitializeFirebase()
+	{
+		// Initialize Firebase
+		FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+
+			// If we are available, Upload the image
+			if (task.Result == DependencyStatus.Available)
+				InitializeStorage();
+			else
+				Debug.LogError($"Could not resolve all Firebase dependencies: {task.Result}");
+		});
+	}
+
+	private void InitializeStorage()
+	{
+		// Attempt to initialize our storage settings
+		storage = FirebaseStorage.DefaultInstance;
+		bucket = storage.GetReferenceFromUrl(_bucketPath);
+		storage.LogLevel = _loggingLevel;
+
+		// Then Immediately begin our upload
+		UploadScreenshot();
+	}
+
+	#endregion
+
+	#region Image Uploading
+
+	private void UploadScreenshot()
+	{
+		// Get our texture for uploading
+		Texture2D temporaryScreenshot = _screenshotCamera.TakeScreenshot(_screenshotRes.x, _screenshotRes.y);
+
+		// Then just upload our image to server
+		StorageReference imgStorageRef = bucket.Child(_destinationPath);
+		MetadataChange imageMetadata = new MetadataChange() { ContentType = "image/png" };
+
+		// Put our file
+		Debug.Log("Starting upload of file to server");
+		imgStorageRef.PutBytesAsync(temporaryScreenshot.EncodeToPNG(), imageMetadata).ContinueWithOnMainThread(task => {
+
+			// Then just log if we completed or failed
+			if (task.IsCanceled || task.IsFaulted)
+				Debug.LogError($"Failed to put file, encountered error '{task.Exception}'");
+			else
+				Debug.Log($"Successfully uploaded file to servers");
+
+			// Then Dispose the task and the texture
+			task.Dispose();
+			Destroy(temporaryScreenshot);
+		});
+	}
+
+	#endregion
+
+}
---
+++ Assets/Scripts/ScreenshotCamera.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class which just references a camera, and forces a screenshot. Returning the result as a Texture2D
+/// </summary>
+public class ScreenshotCamera : MonoBehaviour
+{
+
+	#region Variables
+
+	[Header("Required")]
+	[SerializeField] private Camera _camera;
+
+	#endregion
+
+	#region OnEnable / OnDisable
+
+	private void OnEnable()
+	{
+		if (_camera == null)
+			_camera = GetComponent<Camera>();
+
+		_camera.enabled = false;
+	}
+
+	#endregion
+
+	#region Take Screenshot
+
+	/// <summary>
+	/// Takes a screenshot of the provided camera with the provided settings
+	/// </summary>
+	/// <returns></returns>
+	public Texture2D TakeScreenshot(int width, int height)
+	{
+		// Setup RenderTexture
+		RenderTexture screenshotTemp = RenderTexture.GetTemporary(new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 24));
+		RenderTexture cachedTex = RenderTexture.active;
+		RenderTexture.active = screenshotTemp;
+
+		// Clear our Render Texture
+		GL.Clear(true, true, Color.clear);
+
+		// Setup camera
+		_camera.targetTexture = screenshotTemp;
+		_camera.aspect = width / (float)height;
+
+		// Render
+		_camera.Render();
+
+		// Reset our camera
+		_camera.targetTexture = null;
+		_camera.ResetAspect();
+
+		// Then cast our data and return as a texture 2D
+		Texture2D screenshotTex = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		screenshotTex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+		screenshotTex.Apply();
+
+		// Reset our Render Texture
+		RenderTexture.active = cachedTex;
+		RenderTexture.ReleaseTemporary(screenshotTemp);
+
+		// THen return our texture
+		return screenshotTex;
+	}
+
+	#endregion
+
+}
